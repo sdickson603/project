5e392604f5fc1c0acd3d27357df82b332c3902f7
sdickson603 <you@example.com>
2024-04-24 12:32:08 -0400

    stage07 complete

--- server.js
+++ server.js
@@ -64,3 +64,22 @@ app.get("/customers/:id", async (req, res) => {
       res.send(err);
   }   
 });
+
+app.put('/customers/:id', async (req, res) => {
+  const id = req.params.id;
+  const updatedCustomer = req.body;
+  if (updatedCustomer === null || Object.keys(updatedCustomer).length === 0) {
+      res.status(400);
+      res.send("missing request body");
+  } else {
+      delete updatedCustomer._id;
+      // return array format [message, errMessage]
+      const [message, errMessage] = await da.updateCustomer(updatedCustomer);
+      if (message) {
+          res.send(message);
+      } else {
+          res.status(400);
+          res.send(errMessage);
+      }
+  }
+});
